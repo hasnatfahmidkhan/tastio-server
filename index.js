5fa2bd8658b4a32ee8a928a3a7ec8097dc937af3
hasnatfahmidkhan <hasnatfahmidkhan@gmail.com>
2025-11-09 23:11:45 +0600

    added all reviews and latest reviews

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+
+// middle ware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qk2ebsj.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const tastioDB = client.db("tastioDB");
+    const usersCollection = tastioDB.collection("users");
+    const reviewsCollection = tastioDB.collection("reviews");
+
+    // user insert into db api
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const filter = { email: newUser.email };
+      const isExits = await usersCollection.findOne({ filter });
+      if (isExits) {
+        return res.send({ message: "user already exits" });
+      } else {
+        const result = await usersCollection.insertOne({ ...newUser });
+        res.status(200).send(result);
+      }
+    });
+
+    //? Reviews Get Api
+    // top reviews api
+    app.get("/latest-reviews", async (req, res) => {
+      const result = await reviewsCollection
+        .find()
+        .sort({ postedAt: -1 })
+        .limit(6)
+        .toArray();
+      res.status(200).send(result);
+    });
+
+    // all reviews api
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection
+        .find()
+        .sort({ postedAt: -1 })
+        .toArray();
+      res.status(200).send(result);
+    });
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Tastio server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Tatio is running on ${port}`);
+});
